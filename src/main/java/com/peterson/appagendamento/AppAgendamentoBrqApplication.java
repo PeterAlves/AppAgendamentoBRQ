1336a13256da977f61803ef41ab3f65fcd4a2906
PeterAlves <peterhospdis@gmail.com>
2021-05-13 17:59:38 -0300

    primeiro commit do projeto criado

---
+++ src/main/java/com/peterson/appagendamento/AppAgendamentoBrqApplication.java
@@ -0,0 +1,13 @@
+package com.peterson.appagendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAgendamentoBrqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAgendamentoBrqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peterson/appagendamento/AppAgendamentoBrqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterson.appagendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAgendamentoBrqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
